0b82632e4d95daea43d0de06998db99d862759ac
SecondPsycho <cordell.j.king@gmail.com>
2023-02-28 16:53:46 -0500

    Added Features page

---
+++ Pages/Features.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KingsCorner.Pages;
+
+public class FeaturesModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public FeaturesModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+}
